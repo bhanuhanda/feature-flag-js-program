f954748605b3685dd42ab1af0bd31feaca93fb12
Bhanu Handa <32426959+bhanuhanda@users.noreply.github.com>
2023-06-12 10:04:02 +0530

    caching pending

--- featureflag.js
+++ featureflag.js
@@ -3,43 +3,73 @@ function fetchAllFeatures() {
     // in reality, this would have been a `fetch` call:
     // `fetch("/api/features/all")`
     return new Promise(resolve => {
-      const sampleFeatures = {
-        "extended-summary": true,
-        "feedback-dialog": false
-  };
+        const sampleFeatures = {
+            "extended-summary": 250,
+            "feedback-dialog": 'Your feedbacks would be highly welcomed'
+        };
       setTimeout(resolve, 100, sampleFeatures);
     });
-  }
+}
+
+let globalCache = {}
+let devOrderrides = {}
+addDevOrderride('feedback-dialog', 'test1');
+
+var fetchedAlready = false;
 
 function getFeatureState(flagKey) {
-    return new Promise(function(resolve, reject) {
-        fetchAllFeatures().then(features => {
-            if(Object.keys(features).includes(flagKey)) {
-                resolve(features[flagKey]);
+    return new Promise(function (resolve, reject) {
+        if(!fetchedAlready) {
+            console.log('sent from network');
+            fetchedAlready = false; // set true to enable caching
+
+            fetchAllFeatures().then(features => {
+                console.log({ features })
+                globalCache = {...features, ...devOrderrides};
+                console.log({ globalCache })
+
+                if(globalCache[flagKey]) {
+                    resolve(globalCache[flagKey]);
+                }
+                reject(false);
+            }).catch(function () {
+                // handling network failure -> return from cache
+                console.log('sent from cache', { globalCache });
+                if(globalCache[flagKey]) {
+                    resolve(globalCache[flagKey]);
+                }
+                reject(false);
+            });
+        } else {
+            // handling caching
+            console.log('sent from cache', { globalCache });
+            if(globalCache[flagKey]) {
+                resolve(globalCache[flagKey]);
             }
-            reject('not a valid flag key');
-        })
-    });
+            reject(false);
+        }
+    })
+}
+
+function addDevOrderride(flag, value) {
+    // globalCache = { ...globalCache, [flag] : value }
+    devOrderrides[flag] = value;
 }
 
 // src/feature-x/summary.js
 getFeatureState("extended-summary")
-.then(function(isEnabled) {
-  if (isEnabled) {
-    // showExtendedSummary();
-    console.log('showExtendedSummary()');
-  } else {
-    // showBriefSummary();
-    console.log('showBriefSummary()');
-  }
+.then(function(featureValue) {
+    console.log(featureValue);
+}).catch(function() {
+    console.log('flag key does not exist');
 });
+
 // src/feature-y/feedback-dialog.js
 getFeatureState("feedback-dialog")
-.then(function(isEnabled) {
-  if (isEnabled) {
-    // makeFeedbackButtonVisible();
-    console.log('makeFeedbackButtonVisible()');
-  } else {
-    console.log('makeFeedbackButtonInvisible()')
-  }
+.then(function(featureValue) {
+  console.log(featureValue);
+}).catch(function () {
+    console.log('flag key does not exist');
 });
+
+addDevOrderride('extended-summary', 'test2');
